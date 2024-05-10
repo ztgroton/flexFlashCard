304a50fcdaf201a14068d32a35d01b9611cf84e0
ztgroton <ztgroton@gmail.com>
2024-05-10 11:02:47 -0400

    refactor: modified implementation of 'build_card_html'
    Changed layout of html tags produced from flash card data.

--- R/util_build_card_html.R
+++ R/util_build_card_html.R
@@ -16,14 +16,14 @@ build_card_html <- function(df) {
   stopifnot(isTRUE(nrow(df) == 1))
 
   front <- c(
-    paste('<br>','<br>','<br>', df$Topic),
-    paste('<h1>', df$Question,'</h1>')
+    paste(c(rep('<br>', 7), df$Topic)),
+    paste('<h1>', df$Question,'<h1>')
   )
 
   back <- c(
-    paste('<h2><b><i>',"Definition",'</i></b></h2>', '<h4>', paste(df$Definition), '</h4>'),
-    paste('<h2><b><i>',"Example(s)",'</i></b></h2>', '<h4>', paste(df$Examples), '</h4>'),
-    paste('<h2><b><i>',"Comments / Strategies",'</i></b></h2>', '<h4>', paste(df$Comments), '</h4>')
+    paste('<h2><b><i>',"Definition",'</i></b></h2> <h3>', paste(df$Definition), '</h3>'),
+    paste('<h2><b><i>',"Example(s)",'</i></b></h2> <h3>', paste(df$Examples), '</h3>'),
+    paste('<h2><b><i>',"Comments / Strategies",'</i></b></h2> <h3>', paste(df$Comments), '</h3>')
   )
 
   res <- list(front = front, back = back)
